a5a995008fa8d30210e07cd9f0643b0800a81c19
Joni Mertoniemi <joni.mertoniemi@gmail.com>
2026-03-10 14:29:20 +0200

    chore: Add tests

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,64 +1,68 @@
 import zxcvbn from './main';
-import { Result, Warning } from './types';
+import { Warning } from './types';
 
 describe('zxcvbn', () => {
-  describe('with "password"', () => {
-    let result: Result;
-
-    beforeEach(() => {
-      result = zxcvbn('Password');
-    });
-
-    it('should have really low score', () => {
-      expect(result.score).toBe(0);
-    });
+  it('should have really low score for "password"', () => {
+    const result = zxcvbn('password');
+    expect(result.score).toBe(0);
   });
 
-  describe('with "TDvNM%YK7a46@47p"', () => {
-    let result: Result;
-
-    beforeEach(() => {
-      result = zxcvbn('TDvNM%YK7a46@47p');
-    });
-
-    it('should have really high score', () => {
-      expect(result.score).toBe(4);
-    });
+  it('should have really high score for "TDvNM%YK7a46@47p"', () => {
+    const result = zxcvbn('TDvNM%YK7a46@47p');
+    expect(result.score).toBe(4);
   });
 
-  it('should survive long passowrds', () => {
+  it('should work for long passwords', () => {
     const result = zxcvbn(
       'sadfpokasdfopkpasodfpkoadfskopokpasdfopkadsfpkopokfasdkopfakopkfopdsa'
     );
-
     expect(result).toBeDefined();
   });
 
-  describe('with "Salasana11', () => {
-    let result: Result;
-
-    beforeEach(() => {
-      result = zxcvbn('Salasana11');
-    });
-
-    it('should have low score', () => {
-      expect(result.score).toBe(2);
-    });
-
-    it('should have warning', () => {
-      expect(result.feedback.warning).toBe(Warning.SimilarToCommonPassword);
-    });
+  it('should have low score for "Salasana11" with warning', () => {
+    const result = zxcvbn('Salasana11');
+    expect(result.score).toBe(2);
+    expect(result.feedback.warning).toBe(Warning.SimilarToCommonPassword);
   });
 
-  describe('with "Joni"', () => {
-    let result: Result;
+  it('should have warning about name', () => {
+    const result = zxcvbn('Joni');
+    expect(result.feedback.warning).toBe(Warning.Name);
+  });
 
-    beforeEach(() => {
-      result = zxcvbn('Joni');
-    });
+  it('should have warning about spatial straight row', () => {
+    const result = zxcvbn('fghjkl11');
+    expect(result.score).toBe(1);
+    expect(result.feedback.warning).toBe(Warning.SpatialStraightRow);
+  });
 
-    it('should have warning about name', () => {
-      expect(result.feedback.warning).toBe(Warning.Name);
-    });
+  it('should have warning about repeat pattern', () => {
+    const result = zxcvbn('jkljkljkljkljkljkl');
+    expect(result.score).toBe(0);
+    expect(result.feedback.warning).toBe(Warning.RepeatPattern);
+  });
+
+  it('should have warning about a year', () => {
+    const result = zxcvbn('1991');
+    expect(result.score).toBe(0);
+    expect(result.feedback.warning).toBe(Warning.RegexYear);
+  });
+
+  it('should have warning about a date', () => {
+    const result = zxcvbn('11-11-1991');
+    expect(result.score).toBe(1);
+    expect(result.feedback.warning).toBe(Warning.Date);
+  });
+
+  it('should have warning about repeated characters', () => {
+    const result = zxcvbn('aaaaaa');
+    expect(result.score).toBe(0);
+    expect(result.feedback.warning).toBe(Warning.RepeatCharacter);
+  });
+
+  it('should have warning about a sequence', () => {
+    const result = zxcvbn('abcdef');
+    expect(result.score).toBe(0);
+    expect(result.feedback.warning).toBe(Warning.Sequence);
   });
 });
